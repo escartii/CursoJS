2d2ac7519a9c4708340b09b9d7f7060c43b268ce
escartii <escartismx@gmail.com>
2024-07-11 22:43:04 +0200

    [] - Tipos de datos JS

---
+++ tiposDeDatos.js
@@ -0,0 +1,17 @@
+let miEntero = 10;
+console.log("Mi entero es: " + miEntero);
+
+let numFloat = 1.1;
+console.log("Mi numero flotante es: " + numFloat);
+
+let miNombre = "Alvaro";
+console.log("Bienvenido al programa: " + miNombre);
+
+let miBooleano = true; // true o false
+console.log("Mi booleano es: " + miBooleano);
+
+let miNulo = null;
+console.log("Mi nulo es: " + miNulo);
+
+let miUndefined = undefined; // ausencia de valor
+console.log("Mi undefined es: " + miUndefined);
