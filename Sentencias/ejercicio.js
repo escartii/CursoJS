f8fb9e2628b7059704425f5f2bffe7c7d6132b44
Alvaro Escarti <escartismx@gmail.com>
2024-08-22 21:50:36 +0200

    [] - Curso js

---
+++ Sentencias/ejercicio.js
@@ -0,0 +1,10 @@
+// Algoritmo Mayor edad
+
+const EDAD_ADULTO = 18;
+let edadPersona = 30;
+
+if (edadPersona >= EDAD_ADULTO) {
+    console.log(`La persona con edad: ${edadPersona} es un adulto`)
+} else {
+    console.log(`la persona con edad: ${edadPersona} es menor`)
+}
